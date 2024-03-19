567b517496311509fc7a549cdbd30354c8453f0d
JanSharp <steinmeister36@gmail.com>
2024-03-20 00:05:08 +0100

    Add OcclusionPortalsToggleArea

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Little utilities to help with occlusion, specifically occlusion portals.
 # Installing
 
 Head to my [VCC Listing](https://jansharp.github.io/vrc/vcclisting.xhtml) and follow the instructions there.
+
+# Occlusion Portals
+
+A few things about `Occlusion Portals` components:
+
+- They do not need any mesh renderers
+- They do not need either of the static occluder nor static occludee flags set
+- They do not need colliders (see next section)
+
+So basically they act completely independently from anything else on the object.
+
+In terms of functionality they behave just like a box mesh with the dimensions of the portal, with the added feature of being able to toggle it on and off at runtime. This feature appears to be truly additive without negatively affecting its effectiveness, neither increasing the occlusion data size by any noticeable amount.
+
+# Colliders
+
+If you ever thought that occlusion took colliders into consideration, now you'll know that this is in fact not the case. It's just Mesh Renderers and Occlusion Portals.
+
+# Utils
+
+## Occlusion Portals Toggle Area
+
+Open and close an array of `Occlusion Portal`s within an area. When in the area, they are open, when outside, they are closed.
+
+The area is defined by 1 or more **trigger** colliders of any kind on the object with the `Occlusion Portals Toggle Area` script.
---
+++ Runtime/OcclusionPortalsToggleArea.cs
@@ -0,0 +1,56 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class OcclusionPortalsToggleArea : UdonSharpBehaviour
+    {
+        [Tooltip("When the player is inside of trigger colliders that are on this game objects, the portals "
+            + "will be open. Multiple colliders are supported.\n"
+            + "The default open state of all these portals will be overwritten on Start."
+        )]
+        public OcclusionPortal[] portals;
+        private int triggerCount;
+
+        private void Start()
+        {
+            Close();
+        }
+
+        private void Open()
+        {
+            foreach (OcclusionPortal portal in portals)
+                if (portal != null)
+                    portal.open = true;
+        }
+
+        private void Close()
+        {
+            foreach (OcclusionPortal portal in portals)
+                if (portal != null)
+                    portal.open = false;
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (!player.isLocal)
+                return;
+            triggerCount++;
+            if (triggerCount == 1)
+                Open();
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            // Checking for zero to handle cases where we didn't get the enter event.
+            if (!player.isLocal || triggerCount == 0)
+                return;
+            triggerCount--;
+            if (triggerCount == 0)
+                Close();
+        }
+    }
+}
